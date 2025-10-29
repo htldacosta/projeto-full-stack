4bf9631931c3eb76553d4a828b4be94e42a09f22
htldacosta <hitaludcosta@gmail.com>
2025-10-28 21:48:30 -0400

    criacao de metodo para rota de cadastro de produtos

--- src/main/java/com/produto/api/controller/ProdutoController.java
+++ src/main/java/com/produto/api/controller/ProdutoController.java
@@ -3,15 +3,19 @@ package com.produto.api.controller;
 import com.produto.api.model.ProdutoModel;
 import com.produto.api.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProdutoController {
 
     @Autowired
     private ProdutoService produtoService;
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModel produtoModel) {
+        return produtoService.cadastrar(produtoModel);
+    }
 
     @GetMapping("/Listar")
     public Iterable<ProdutoModel> listar(){
